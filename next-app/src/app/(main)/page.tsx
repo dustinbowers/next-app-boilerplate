8a25062667160e7af2a765aaa2aaedde20d493ed
Dustin Bowers <dustin.uncc@gmail.com>
2024-06-27 17:19:44 -0400

    update initial directory structure

---
+++ next-app/src/app/(main)/page.tsx
@@ -0,0 +1,16 @@
+// "use client";
+
+const IndexPage = async () => {
+
+    return (
+        <>
+            <main className="flex min-h-screen flex-col items-center justify-between p-24">
+                <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+                    Hello boilerplate
+                </div>
+            </main>
+        </>
+    );
+}
+
+export default IndexPage;
